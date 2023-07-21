5f1c487b386139edc40bd5bf1ee3df503bab327e
Samuel Adlard <72796569+SamuelAdlard@users.noreply.github.com>
2023-07-21 14:59:02 +1000

    HGelpo

--- MorseCode/MorseCodeScript.js
+++ MorseCode/MorseCodeScript.js
@@ -18,25 +18,47 @@ const morseCodeTable ={
   "Q": "--.-",
   "R": ".-.",
   "S": "...",
-  "T": "- ",
+  "T": "-",
   "U": "..-",
   "V": "...-",
   "W": ".--",
   "X": "-..-",
   "Y": "-.--",
   "Z": "--..",
-  " ": "/"
+  " ": "/",
+  ".":"/... - --- .--."
 }
 
 function Translate()
 {
-  const inputText = document.getElementById("textToTranslate").value;
-  alert(inputText);
+  const inputText = document.getElementById("textToTranslate").value.toUpperCase();
+  
   let outputText = "";
   for(let i = 0; i < inputText.length; i++)
   {
     outputText += morseCodeTable[inputText[i]] + " ";
   }
 
+  document.getElementById("Output").innerHTML=outputText;
   console.log(outputText);
+  GenerateAudio(outputText);
+}
+
+function GenerateAudio(outputText)
+{
+  const timeTable = {
+    
+  }
+}
+
+
+function PlayAudio()
+{
+  var context = new (window.AudioContext || window.webkitAudioContext)();
+  var osc = context.createOscillator(); // instantiate an oscillator
+  osc.type = 'sine'; // this is the default - also square, sawtooth, triangle
+  osc.frequency.value = 550; // Hz
+  osc.connect(context.destination); // connect it to the destination
+  osc.start(); // start the oscillator
+  osc.stop(context.currentTime + 2); // stop 2 seconds after the current time
 }
