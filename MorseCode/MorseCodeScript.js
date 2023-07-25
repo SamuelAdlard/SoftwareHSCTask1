c27863fdf54c98383c2ff95f1e26a8fb78eca7cb
Samuel Adlard <72796569+SamuelAdlard@users.noreply.github.com>
2023-07-25 10:06:06 +1000

    Sound playing working and comments added

--- MorseCode/MorseCodeScript.js
+++ MorseCode/MorseCodeScript.js
@@ -1,4 +1,4 @@
-const morseCodeTable ={
+const morseCodeTable ={ //Table that contains all the morse code character values
   "A": ".-",
   "B": "-...",
   "C": "-.-.",
@@ -39,33 +39,32 @@ const morseCodeTable ={
   "9": "----."
 }
 
-const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-const oscillator = audioContext.createOscillator();
-oscillator.connect(audioContext.destination);
-oscillator.frequency.value = 440; // 440 Hz is the frequency of the note A
-oscillator.type = 'sine';
 
 
 
 function Translate()
 {
+  //gets the input of the user
   const inputText = document.getElementById("textToTranslate").value.toUpperCase();
-  
+  //clears the output string to allow more values to be added
   let outputText = "";
   for(let i = 0; i < inputText.length; i++)
   {
-    outputText += morseCodeTable[inputText[i]] + " ";
+    //loops through all the characters entered in the normal alphabet and translates them to morse code
+    outputText += morseCodeTable[inputText[i]] + " "; //adds a space onto the end 
   }
 
+  //Displays the translated text on screen
   document.getElementById("Output").innerHTML=outputText;
   console.log(outputText);
   
-  for(let i = 1; i <= outputText.length; i++)
+  //Loops through the output and plays the appropriate sound
+  for(let i = 0; i < outputText.length; i++)
   {
     setTimeout(() =>{
     
-      GenerateAudio(".",0);
-    }, 1000 * i) //put this in a for loop and multiply the time by the index
+      GenerateAudio(outputText,i);//Calls the function that plays the current morse code character
+    }, 1000 * i) //Waits before playing each sound
   }
 
   
@@ -76,31 +75,45 @@ function Translate()
 
 function GenerateAudio(outputText, index)
 {
-  
+  //gets the character that is being worked on
   const character = outputText[index];
+  //creates a new audio context
   const audioContext = new AudioContext();
+  //creates a new oscillator
   const oscillator = audioContext.createOscillator();
+  //sets the oscillator type to sine
   oscillator.type = 'sine';
+  //connects the oscillator to the speakers 
   oscillator.connect(audioContext.destination);
-  oscillator.start();
-
   
+  
+  //Doesn't play a sound if the character is a / or a space
+  if(character != "/" && character != " "){
+    //starts playing a sound
+    oscillator.start();
+    console.log(`Started! ${index} Character playing ${character}`);
+  }
+  else if(character == "/"){ //Waits a certain time to return between words
+    setTimeout(() => {
+      return;
+    }, 1400);
+  }
+  
+  //Stops the sound at the right time to make it a dit
   if(character == ".")
   {
     setTimeout(() => {
       oscillator.stop();
+      console.log(`Ended! ${index} Character playing ${character}`);
     }, 200);
   }
+  //Stops the sound at the right time to make it a dah
   else if(character == "-")
   {
     setTimeout(() => {
       oscillator.stop();
-    }, 400);
+      console.log("Ended");
+    }, 600);
   }
-  
-  
-
-  
-
 }
 
