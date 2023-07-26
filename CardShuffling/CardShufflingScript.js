91a9703d082a332dd353db3caf41e87112558c62
Samuel Adlard <72796569+SamuelAdlard@users.noreply.github.com>
2023-07-26 14:56:54 +1000

    hello

--- CardShuffling/CardShufflingScript.js
+++ CardShuffling/CardShufflingScript.js
@@ -1,11 +1,12 @@
 const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
 const ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+const images = ["image1", "image2", "image3", "image4", "image5"];
 
 let deck = [];
 
 for (let suitIndex = 0; suitIndex < suits.length; suitIndex++) {
   for (let rankIndex = 0; rankIndex < ranks.length; rankIndex++) {
-    let card = ranks[rankIndex] + " of " + suits[suitIndex];
+    let card = ranks[rankIndex] + "_of_" + suits[suitIndex];
     deck.push(card);
   }
 }
@@ -14,5 +15,6 @@ document.getElementById("displayText").innerHTML = deck;
 
 function ShuffleDeck()
 {
-    
+  const shuffledArray = deck.sort((a, b) => 0.5 - Math.random());
+  document.getElementById("displayText").innerHTML = shuffledArray;
 }
