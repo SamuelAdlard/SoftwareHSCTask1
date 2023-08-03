20a25370e1b09bcda4a466e0fc2b321812fc43c6
Samuel Adlard <72796569+SamuelAdlard@users.noreply.github.com>
2023-08-03 13:39:54 +1000

    Finished

--- Magic8Ball/8Ball.js
+++ Magic8Ball/8Ball.js
@@ -1,11 +1,26 @@
+const responses = [" It is certain."," It is decidedly so.","Without a doubt.","You may rely on it.", "As I see it, yes.","Most likely.","Outlook good."," Yes.","Signs point to yes.",
+                        "Reply hazy, try again."," Ask again later."," Better not tell you now.","Cannot predict now."," Concentrate and ask again.", "Don't count on it.", "My reply is no."
+                        ," My sources say no.","Outlook not so good.","Very doubtful."];
+
+
+
+
+
+
 function GenerateAnswer()
 {
     //array containing all possible responses
-    const responses = [" It is certain."," It is decidedly so.","Without a doubt.","You may rely on it.", "As I see it, yes.","Most likely.","Outlook good."," Yes.","Signs point to yes.",
-                        "Reply hazy, try again."," Ask again later."," Better not tell you now.","Cannot predict now."," Concentrate and ask again.", "Don't count on it.", "My reply is no."
-                        ," My sources say no.","Outlook not so good.","Very doubtful."];
+    
+    document.getElementById("ResponseBox").innerHTML= "Thinking...."
+    setTimeout(() => {
+        ShowResponse();
+    }, 1000); 
+}
+
+function ShowResponse()
+{
     //Sets the text of the answer box to a random response
-    document.getElementById("ResponseBox").innerHTML= responses[GenerateRandomNumber(responses.length)];    
+    document.getElementById("ResponseBox").innerHTML= responses[GenerateRandomNumber(responses.length)]; 
 }
 
 //Function for generating random numbers
